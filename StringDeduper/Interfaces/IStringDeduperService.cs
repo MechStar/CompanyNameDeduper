38cbc937cb5ae7ab41a91fd01e5b3d100c38a8a7
Denis Pavlov <info@denispavlov.com>
2024-02-26 07:59:04 -0800

    Add extensive comments

--- StringDeduper/Interfaces/IStringDeduperService.cs
+++ StringDeduper/Interfaces/IStringDeduperService.cs
@@ -3,6 +3,6 @@ namespace StringDeduper.Interfaces;
 public interface IStringDeduperService
 {
     Task ImportStrings(IAsyncEnumerable<string> input);
-    IEnumerable<string> GetDuplicates(bool includeOriginal = false, bool excludeRepeats = false, bool skipLine = false);
+    IEnumerable<string> GetDuplicates(bool includeOriginal = false, bool excludeRepeats = false, bool addEmptyString = false);
     IEnumerable<string> GetUniques(bool restrictToUniqueInput = false);
 }
